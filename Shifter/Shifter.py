e3cfd9c463de1aa61c2a6d8435f0ad95d5306510
Eiqram <151066160+EiqramNas@users.noreply.github.com>
2024-12-24 00:06:05 +0800

    Update Shifter.py

--- Shifter/Shifter.py
+++ Shifter/Shifter.py
@@ -3,6 +3,8 @@ import pydirectinput as pyd
 import time
 import ctypes
 from ctypes import windll
+from pygame import mixer
+import threading
 
 user32=ctypes.windll.user32
 chip=input('Board: ')
@@ -13,7 +15,14 @@ pin9=board.get_pin("d:9:i")
 it=pyf.util.Iterator(board)
 it.start()
 
-codes={"E": 0x45, "Q": 0x51}
+codes={"E": 0x45, "Q": 0x51, "P": 0x50}
+
+def ShiftingSound():
+    mixer.init()
+    mixer.music.load('Explode.mp3')
+    mixer.music.play()
+    while mixer.music.get_busy():
+        time.sleep(0.5)
 
 def fast_press(key):
     code=codes.get(key)
@@ -21,25 +30,50 @@ def fast_press(key):
     user32.keybd_event(key,0,2,0)
     print(key)
 
+def ParkingBrake():
+    while True:
+        press=False
+        if pin9.read()==1:
+            press=True
+        if press==False:
+            pyd.keyUp("p")
+        if press==True:
+            pyd.keyDown("p")
+        time.sleep(.05)
+
+thread=threading.Thread(target=ParkingBrake)
+thread.start()
+
 def Main():
     while True:
         if pin7.read()==1:
+            current=time.time()
+            print(current)
             #fast_press("Q")
+            print("Clutch Down")
             pyd.keyDown("shift")
+            print("Shifting Down")
             pyd.press("q")
             pyd.keyUp("shift")
+            print("Clutch Up")
             #pyd.press("f")
             time.sleep(.05)
         if pin8.read()==1:
+            current=time.time()
+            print(current)
             #fast_press("E")
+            print("Clutch Down")
             pyd.keyDown("shift")
+            print("Shifting Up")
             pyd.press("e")
             pyd.keyUp("shift")
+            print("Clutch Up")
             #pyd.press("r")
             time.sleep(.05)
-        if pin9.read()==1:
-            pyd.press("f")
-            time.sleep(.05)
-
+        #if pin9.read()==1:
+        #    pyd.press("f")
+        #    time.sleep(.05)
+        time.sleep(0.05)
+        
 if __name__ == '__main__':
     Main()
